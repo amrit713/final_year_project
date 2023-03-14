97bce5d409900bc178bdc40854f08568b8506292
amrit713 <amritghimire741443@gmail.com>
2023-03-14 09:31:57 +0545

    server Error, uncounght Expection and memory leak risk error handled

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,14 @@ config({ path: path.resolve(__dirname, "../config.env") });
 
 import app from "./app";
 
+//handling uncaught error Exception  not define error
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log("Shutting Down the  server due to uncaught Exception ");
+
+    process.exit(1);
+});
+
 const database = `${process.env.DATABASE_URL}`;
 
 mongoose.set("strictQuery", false);
@@ -17,6 +25,18 @@ mongoose.connect(database).then(() => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, (): void => {
+const server =app.listen(PORT, (): void => {
     console.log(`server is running on http://localhost:${PORT}`);
+});
+
+//unHandle memory leak risk
+process.on("warning", (e) => console.warn(e.stack));
+
+//Handle unhandled promise rejection
+process.on("unhandledRejection", (err: any) => {
+    console.log(`Error ${err.message}`);
+    console.log("Shutting down the server due to unhandled Promise rejection")
+    server.close(() => {
+        process.exit(1);
+    });
 });
